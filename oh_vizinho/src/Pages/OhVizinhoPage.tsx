2a467b530382f69079406486297586281a9ea41a
Sebastião Martins <ssa.martins@campus.fct.unl.pt>
2024-11-16 19:30:45 +0000

    fix no menu lateral, mudar style, apresentar o nome de utilizador, fechar ao clicar fora, burger button

--- oh_vizinho/src/Pages/OhVizinhoPage.tsx
+++ oh_vizinho/src/Pages/OhVizinhoPage.tsx
@@ -13,6 +13,7 @@ import CreateProduct from './CreateProduct';
 type CardType = 'product' | 'recipe' | 'order' | 'Perfil' | 'Mensagens' | 'Meus Pedidos' | 'Minhas Ofertas' | 'Dispensa';
 
 const OhVizinhoPage: React.FC = () => {
+  const [userName, setUserName] = useState<string | null>(null);
   const [showPopup, setShowPopup] = useState(false);
   const [viewType, setViewType] = useState<CardType>('recipe');
   const [query, setQuery] = useState<Query>({
@@ -22,7 +23,7 @@ const OhVizinhoPage: React.FC = () => {
     spicy: false,
     glutenFree: false,
     lactoseFree: false,
-    vegan: false
+    vegan: false,
   });
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -42,16 +43,14 @@ const OhVizinhoPage: React.FC = () => {
     setViewType(type);
   };
 
-  const toogleCreatePopup = () =>  {
-    if(viewType == 'product')
-      setCreateProductPopupOpen(!isCreateProductPopupOpen);
-    else
-      setCreateOrderPopupOpen(!isCreateOrderPopupOpen);
+  const toggleCreatePopup = () => {
+    if (viewType === 'product') setCreateProductPopupOpen(!isCreateProductPopupOpen);
+    else setCreateOrderPopupOpen(!isCreateOrderPopupOpen);
   };
 
   const createProduct = (productData: any) => {
-      setProducts([...products, productData]); 
-      setCreateProductPopupOpen(false); 
+    setProducts([...products, productData]);
+    setCreateProductPopupOpen(false);
   };
 
   const getItemsByType = () => {
@@ -70,42 +69,51 @@ const OhVizinhoPage: React.FC = () => {
         return pantry;
       case 'Perfil':
       case 'Mensagens':
-      default: 
+      default:
         return [];
     }
   };
 
   const handleFilterNameChange = (filterName: string) => {
-    setQuery({...query, name: filterName})
+    setQuery({ ...query, name: filterName });
   };
 
   const handleFilterChange = (filter: Query) => {
     const name = query.name;
-    setQuery({...filter, name});
+    setQuery({ ...filter, name });
   };
 
   const handleSideBarClick = () => {
     setSideBar(!sideBar);
-  }
+  };
 
   const handleLogout = () => {
     setIsAuthenticated(false);
-  }
+    //setUserName(null);
+  };
+
 
   return (
     <div data-layername="base" className="flex overflow-hidden flex-col items-center pt-4 bg-white pb-[548px] max-md:pb-24 w-full">
-      <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} onSideBar={handleSideBarClick}/>
-      <PageHeading 
-          togglePopup={togglePopup} 
-          onViewChange={handleViewChange} 
-          filterName={handleFilterNameChange} 
-          isAuthenticated={isAuthenticated} 
-          toggleCreatePopup={toogleCreatePopup}
-          currentViewType={viewType}
+      {/* Passamos `sideBar` como prop para o Header */}
+      <Header
+        isAuthenticated={isAuthenticated}
+        setIsAuthenticated={setIsAuthenticated}
+        onSideBar={handleSideBarClick}
+        sideBar={sideBar} // Estado que controla a visibilidade do botão
+        onSetUserName={setUserName}
       />
-      
+      <PageHeading
+        togglePopup={togglePopup}
+        onViewChange={handleViewChange}
+        filterName={handleFilterNameChange}
+        isAuthenticated={isAuthenticated}
+        toggleCreatePopup={toggleCreatePopup}
+        currentViewType={viewType}
+      />
+
       {isAuthenticated || viewType === 'recipe' ? (
-        <ProductGrid items={getItemsByType()} cardType={viewType} query={query}/>
+        <ProductGrid items={getItemsByType()} cardType={viewType} query={query} />
       ) : (
         <div className="flex items-center justify-center h-[50vh] text-center">
           <p className="text-2xl font-semibold text-gray-500">
@@ -113,19 +121,18 @@ const OhVizinhoPage: React.FC = () => {
           </p>
         </div>
       )}
-      
+
       <Filter isOpen={showPopup} onClose={togglePopup} filterType={viewType} onFilterChange={handleFilterChange} />
-      <CreateProduct 
+      <CreateProduct
         isOpen={isCreateProductPopupOpen}
-        onClose={toogleCreatePopup}
+        onClose={toggleCreatePopup}
         create={createProduct}
       />
-      {
-      sideBar && isAuthenticated && (
-      <div className="fixed top-0 right-0 w-[350px] h-full">
-          <MenuCard onMenuItemClick={handleViewChange} logout={handleLogout}/>
-      </div>)
-      }
+      {sideBar && isAuthenticated && (
+        <div className="fixed top-0 right-0 w-[350px] h-full">
+          <MenuCard onMenuItemClick={handleViewChange} logout={handleLogout} closeSideBar={handleSideBarClick} userName={userName}/>
+        </div>
+      )}
     </div>
   );
 };
--- oh_vizinho/src/components/PageComponents/Header.tsx
+++ oh_vizinho/src/components/PageComponents/Header.tsx
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginPage from '../../Pages/LoginPage';
 import Button from '../Button';
 
 interface HeaderProps {
   setIsAuthenticated: (value: boolean) => void;
-  onSideBar: (isClicked: boolean) => void; // Função para emitir o evento booleano
-  isAuthenticated: boolean; // Nova propriedade para verificar se o usuário está autenticado
+  onSideBar: (isClicked: boolean) => void;
+  isAuthenticated: boolean;
+  sideBar: boolean;
+  onSetUserName: (name: string | null) => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ setIsAuthenticated, onSideBar, isAuthenticated }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(isAuthenticated); // Usar a variável isAuthenticated do pai
+const Header: React.FC<HeaderProps> = ({
+  setIsAuthenticated,
+  onSideBar,
+  isAuthenticated,
+  sideBar,
+  onSetUserName,
+}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(isAuthenticated);
   const [userName, setUserName] = useState<string | null>(null);
   const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
 
-  // Sincroniza isLoggedIn com isAuthenticated
   useEffect(() => {
-    setIsLoggedIn(isAuthenticated); // Atualiza isLoggedIn sempre que isAuthenticated mudar
+    setIsLoggedIn(isAuthenticated);
   }, [isAuthenticated]);
 
-  // Sempre que isLoggedIn mudar, atualiza isAuthenticated
   useEffect(() => {
-    setIsAuthenticated(isLoggedIn); // Atualiza o estado no pai sempre que isLoggedIn mudar
+    setIsAuthenticated(isLoggedIn);
   }, [isLoggedIn, setIsAuthenticated]);
 
   useEffect(() => {
@@ -39,9 +45,10 @@ const Header: React.FC<HeaderProps> = ({ setIsAuthenticated, onSideBar, isAuthen
   const handleLogin = (username: string, password: string) => {
     if (username) {
       setIsLoggedIn(true);
+      onSetUserName(username)
       setUserName(username);
       sessionStorage.setItem('authToken', username);
-      setIsAuthenticated(true); // Atualiza o estado global na página
+      setIsAuthenticated(true);
       setIsLoginPopupOpen(false);
     } else {
       alert('Usuário ou senha incorretos.');
@@ -49,15 +56,11 @@ const Header: React.FC<HeaderProps> = ({ setIsAuthenticated, onSideBar, isAuthen
   };
 
   const handleRegister = () => {
-    console.log("Redirecting to registration page...");
+    console.log('Redirecting to registration page...');
   };
 
   const handleClosePopup = () => {
     setIsLoginPopupOpen(false);
-  };
-
-  const handleSideBarClick = () => {
-    onSideBar(true);
   };
 
   return (
@@ -69,24 +72,27 @@ const Header: React.FC<HeaderProps> = ({ setIsAuthenticated, onSideBar, isAuthen
           alt="Oh Vizinho! logo"
           className="object-contain shrink-0 w-20 aspect-square"
         />
-        <div data-layername="ohVizinho" className="my-auto basis-auto">Oh Vizinho!</div>
+        <div data-layername="ohVizinho" className="my-auto basis-auto">
+          Oh Vizinho!
+        </div>
       </div>
 
       <div className="flex gap-6 my-auto text-base leading-tight text-center text-black">
         {isLoggedIn ? (
           <>
-            <div data-layername="userName" className="my-auto basis-auto">{userName}</div>
-            <button
-              onClick={handleSideBarClick}
-              className="p-1 w-[30px] h-[30px] bg-transparent border-0 rounded-full"
-            >
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/def97886f7fc40cbecb9bf0fdc4ff631c9da645840e7351d9cb6d7a9153d683b?placeholderIfAbsent=true&apiKey=2b659d54d9c448a19edda772d8c18782"
-                alt="User profile"
-                className="object-contain w-full h-full rounded-full"
-              />
-            </button>
+            {!sideBar && ( // Exibe o botão somente se sideBar for falso
+              <button
+                onClick={() => onSideBar(true)}
+                className="p-1 w-[40px] h-[40px] bg-transparent border-0 rounded-full transition-opacity"
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/def97886f7fc40cbecb9bf0fdc4ff631c9da645840e7351d9cb6d7a9153d683b?placeholderIfAbsent=true&apiKey=2b659d54d9c448a19edda772d8c18782"
+                  alt="User profile"
+                  className="object-contain w-full h-full rounded-full"
+                />
+              </button>
+            )}
           </>
         ) : (
           <Button onClick={handleLoginClick} primary>
